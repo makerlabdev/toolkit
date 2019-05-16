7f061964986231e22459183c48347c39d8908359
Jori van Keulen <jori@makerlab.nl>
2019-05-16 08:38:28 +0200

    - single utm method

--- msitoolkit.js
+++ msitoolkit.js
@@ -1,15 +1,34 @@
 // MSI TOOLKIT 0.1
 // AUTHORS: JORI VAN KEULEN
 
-// Constructor
 (function(window){
 	function msiToolkit(){
 		var _msiToolkitObject = {};
 
-		// methods
-		_msiToolkitObject.alleUTMs = function() {
+		// Hier schrijven we alle methods!
+
+		// Return alle URL params in 1 string 
+		_msiToolkitObject.alleUTM = function() {
 			var data = window.location.search.slice(1);
 			return data;
+		}
+
+		// Return individuele URL params op basis van naam
+		_msiToolkitObject.losseUTM = function(name) {
+			name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+			var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+			results = regex.exec(location.search);
+			return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+		}
+
+		// Alles in 1 cookiebanner
+		_msiToolkitObject.cookieBanner = function() {
+			return "work in progress....";
+		}
+
+		// Maak per URL param een hidden field in het formulier
+		_msiToolkitObject.maakHiddenFields = function(formID) {
+
 		}
 
 		return _msiToolkitObject;
