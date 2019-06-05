167cd85722b7c3c00b67cb46dec4acb8b35c9c37
Jori van Keulen <jori@makerlab.nl>
2019-06-05 15:18:57 +0200

    maakHiddenFields

--- msitoolkit.js
+++ msitoolkit.js
@@ -28,14 +28,25 @@
 		}
 
 		// Maak per URL param een hidden field in het formulier
-		_msiToolkitObject.maakHiddenFields = function(formID) {
+		_msiToolkitObject.maakHiddenFields = function(formID = "form") {
+			var formObj = $("#"+formID);
+			var utmRaw = location.search.substring(1);
+			var utmObj = JSON.parse('{"' + utmRaw.replace(/&/g, '","').replace(/=/g,'":"') + '"}', function(key, value) { return key===""?value:decodeURIComponent(value) });
 
+			$.each(utmObj, function(key, value) {
+				$('<input>').attr({
+						type: 'hidden',
+						id: key,
+						name: key,
+						value: value,
+				}).appendTo(formObj);
+			});
 		}
 
 		// Vind alle CTA's en plak UTM codes in de href
-		_msiToolkitObject.utmNaarAlleCTA = function(cta = "btn-cta") {
-			var ctaBtnArray = $('*[id^="'+cta+'"]'); 
-			ctaBtnArray.each(function() {
+		_msiToolkitObject.utmNaarAlleCTA = function(ctaID = "btn-cta") {
+			var ctaBtnObj = $('*[id^="' + ctaID + '"]'); 
+			ctaBtnObj.each(function() {
 			    var url = $(this).attr('href');
 			    $(this).attr('href', url + window.location.search.slice(1));
 			});
